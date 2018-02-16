744fe6bb1142e7d1438c3a9fd7605be22554a8b5
Sandeep AR <sandeep.setty@costrategix.com>
2018-02-16 19:18:27 +0530

    pass test

--- phpUnitTutorial/Test/sample2Test.php
+++ phpUnitTutorial/Test/sample2Test.php
@@ -10,7 +10,7 @@ class StackTest2 extends \PHPUnit_Framework_TestCase
 
         array_push($stack, 'foo');
         $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(2, count($stack));
+        $this->assertEquals(1, count($stack));
 
         $this->assertEquals('foo', array_pop($stack));
         $this->assertEquals(0, count($stack));
